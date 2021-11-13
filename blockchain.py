1bb16bd2604ba295dc2d5c3a62fc9a1b00b8839c
SecondPort <gomezmanulm@gmail.com>
2021-11-13 00:34:32 -0300

    add chain_valid

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,7 @@
 import datetime
 import hashlib  # algoritmos de hashes
 import json
-from flask import Flask, jsonify
+from flask import Flask, app, jsonify
 
 # parte 1
 
@@ -36,5 +36,26 @@ class Blockchain:
             if hash_operation[:4] == '0000': #compara el hash con el formato 0000
                 check_proof = True 
             else:
-                new_proof += 1 #si no es igual a 0000, incrementa el numero
+                new_proof += 1 #si no es igual a 0000, incrementa la prueba
         return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps( block, sort_keys=True).encode() #codifica el bloque
+        return hashlib.sha256(encoded_block).hexdigest() #genera un hash
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0] #obtiene el primer bloque
+        block_index = 1 #indice del bloque actual 
+        while block_index < len(chain):
+            block = chain[block_index] #obtiene el bloque actual
+            if block['previous_hash'] != self.hash(previous_block): #compara el hash del bloque anterior con el hash del bloque actual
+                return False
+            previous_proof = previous_block['proof'] #obtiene la prueba del bloque anterior
+            proof = block['proof'] #obtiene la prueba del bloque actual
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest() #genera un hash
+            if hash_operation[:4] != '0000': 
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+    
