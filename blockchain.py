a0ac93f302ca34ada5dbaf4e305f52ab39d0c6db
SecondPort <gomezmanulm@gmail.com>
2021-11-13 01:09:21 -0300

    app flask mine_block

--- blockchain.py
+++ blockchain.py
@@ -59,3 +59,28 @@ class Blockchain:
             block_index += 1
         return True
     
+# parte 2
+
+# crear una aplicacion flask
+app = Flask(__name__)
+
+# crear una blockchain
+blockchain = Blockchain()
+
+# Minar un bloque
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+
